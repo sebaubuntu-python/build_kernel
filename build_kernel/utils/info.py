914b6b237ba48e1f107a0fbd4fff9047287259bc
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-09-30 13:04:19 +0200

    build_kernel: utils: info: Refactor
    Change-Id: I6e55e261f0245151d0288b242be565e52e63576e

--- build_kernel/utils/info.py
+++ build_kernel/utils/info.py
@@ -1,22 +1,16 @@
 from build_kernel import get_config
 from build_kernel.utils.config import Config
 
-TEXT = """\
-============================================
-Linux kernel version={config.kernel_version}
-Architecture={config.arch}
-Sources directory={config.kernel_path}
-Output directory={out_path}
-Build user={build_user}
-Build machine={build_host}
-Toolchain={config.toolchain}
-ccache enabled={ccache}
-============================================
-"""
-
 def print_summary(config: Config):
-	text = TEXT.format(config=config, out_path=config.out_path,
-					   build_user=get_config("KBUILD_BUILD_USER"),
-					   build_host=get_config("KBUILD_BUILD_HOST"),
-					   ccache=get_config("ENABLE_CCACHE"))
-	print(text)
+	print("\n".join([
+		"============================================",
+		f"KERNEL_VERSION={config.kernel_version}",
+		f"TARGET_ARCH={config.arch}",
+		f"TARGET_KERNEL_SOURCES={config.kernel_path}",
+		f"OUT_DIR={config.out_path}",
+		f"BUILD_USER={get_config('KBUILD_BUILD_USER')}",
+		f"BUILD_HOST={get_config('KBUILD_BUILD_HOST')}",
+		f"TOOLCHAIN={config.toolchain}",
+		f"CCACHE={get_config('ENABLE_CCACHE')}",
+		"============================================",
+	]))
