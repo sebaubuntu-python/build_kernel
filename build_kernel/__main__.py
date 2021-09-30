92346730c37aa4b72bb8c7c795022ffe07e75151
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-09-30 12:49:37 +0200

    build_kernel: Refactor main
    Change-Id: I7e88cd3200ba5a4a7b98edbbbf63d27821ebfbf1

--- build_kernel/__main__.py
+++ build_kernel/__main__.py
@@ -1,20 +1,6 @@
 #!/usr/bin/python3
 
-from argparse import ArgumentParser
 from build_kernel.main import main
-from pathlib import Path
 
 if __name__ == '__main__':
-	parser = ArgumentParser(prog='python3 -m kernel_build')
-
-	parser.add_argument("config", type=Path,
-						help="device configuration file")
-
-	parser.add_argument("-c", "--clean", action='store_true',
-						help="clean before building")
-	parser.add_argument("-v", "--verbose", action='store_true',
-						help="verbose logging")
-
-	args = parser.parse_args()
-
-	main(args.config, clean=args.clean)
+	main()
--- build_kernel/main.py
+++ build_kernel/main.py
@@ -1,3 +1,4 @@
+from argparse import ArgumentParser
 from build_kernel.utils.ak3 import AK3Manager
 from build_kernel.utils.cc import Make
 from build_kernel.utils.config import load_config_module
@@ -5,15 +6,26 @@ from build_kernel.utils.info import print_summary
 from build_kernel.utils.logging import LOGI
 from pathlib import Path
 
-def main(config_path: Path, clean=False):
-	config = load_config_module(config_path)
+def main():
+	parser = ArgumentParser(prog='python3 -m kernel_build')
+
+	parser.add_argument("config", type=Path,
+						help="device configuration file")
+	parser.add_argument("-c", "--clean", action='store_true',
+						help="clean before building")
+	parser.add_argument("-v", "--verbose", action='store_true',
+						help="verbose logging")
+
+	args = parser.parse_args()
+
+	config = load_config_module(args.config)
 	make = Make(config)
 
 	print_summary(config)
 
 	(config.out_path / "KERNEL_OBJ").mkdir(exist_ok=True, parents=True)
 
-	if clean is True:
+	if args.clean is True:
 		LOGI("Cleaning before building")
 		make.run("clean")
 		make.run("mrproper")
