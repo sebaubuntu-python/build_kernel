615f9b583b65fe91ba1261bdbb56c15bdf74024c
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-03-22 23:30:54 +0100

    build_kernel: utils: dumpvars: Fix kernel sources path
    Change-Id: If7c6514a3272b497f5fc2c3d9f4b57a183f05a73

--- build_kernel/utils/dumpvars.py
+++ build_kernel/utils/dumpvars.py
@@ -1,4 +1,4 @@
-from build_kernel import kernel_path, out_path
+from build_kernel import current_path, out_path
 from build_kernel.utils.device import Device
 from build_kernel.utils.make import KBUILD_BUILD_USER, KBUILD_BUILD_HOST, ENABLE_CCACHE
 
@@ -6,7 +6,7 @@ def dumpvars(device: Device):
 	vars = {
 		"PRODUCT_DEVICE": device.PRODUCT_DEVICE,
 		"TARGET_ARCH": device.TARGET_ARCH,
-		"TARGET_KERNEL_SOURCE": kernel_path / device.TARGET_KERNEL_SOURCE,
+		"TARGET_KERNEL_SOURCE": current_path / device.TARGET_KERNEL_SOURCE,
 		"OUT_DIR": out_path / device.PRODUCT_DEVICE,
 		"BUILD_USER": KBUILD_BUILD_USER,
 		"BUILD_HOST": KBUILD_BUILD_HOST,
