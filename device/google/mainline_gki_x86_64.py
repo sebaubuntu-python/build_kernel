77a54c78c84194f75f4af8f4eb58656e61d48512
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-04-21 00:50:14 +0200

    device: Add mainline_gki_x86_64
    Change-Id: I19becbfab467bd53c7ef047ae0f4fe5cbf6c229d

---
+++ device/google/mainline_gki_x86_64.py
@@ -0,0 +1,12 @@
+from build_kernel.utils.device import Device, register_device
+
+class GoogleMainlineGkix8664Device(Device):
+	PRODUCT_DEVICE = "mainline_gki_x86_64"
+	TARGET_ARCH = "x86_64"
+	TARGET_KERNEL_CONFIG = "gki_defconfig"
+	TARGET_KERNEL_SOURCE = "kernel/google/android-mainline"
+	TARGET_BLOCK_DEVICE = "/dev/block/by-name/boot"
+	TARGET_BUILD_ARTIFACTS = ["vmlinuz"]
+	TARGET_KERNEL_USE_HOST_COMPILER = True
+
+register_device(GoogleMainlineGkix8664Device)
