00fd61e70cf2b6b9acb54aaf0aefab7ce733e45d
Ultra119 <64977575+Ultra119@users.noreply.github.com>
2022-08-03 23:30:47 +0300

    device: lenovo: sdm710: We don't have a ramdisk in boot
    Tested on latest PE device kernel source (kunlun/jd2019)

--- device/lenovo/sdm710.py
+++ device/lenovo/sdm710.py
@@ -3,4 +3,4 @@ from device.qcom.sdm845 import QcomSDM845Device
 class LenovoSDM710Device(QcomSDM845Device):
 	TARGET_ARCH = "arm64"
 	TARGET_KERNEL_SOURCE = "kernel/lenovo/sdm710"
-	BOARD_BUILD_SYSTEM_ROOT_IMAGE = False
+	BOARD_BUILD_SYSTEM_ROOT_IMAGE = True
