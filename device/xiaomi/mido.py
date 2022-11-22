b6a85dccc4d17c3099b62cab4d4d07384a833965
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-11-22 19:32:55 +0100

    device: xiaomi: msm8953: Bye bye
    Change-Id: I30f492b5b064d18ded0ea52cf4e50201cfe22e6a

--- device/xiaomi/mido.py
+++
@@ -1,8 +0,0 @@
-from build_kernel.utils.device import register_device
-from device.xiaomi.msm8953 import XiaomiMSM8953Device
-
-class XiaomiMidoDevice(XiaomiMSM8953Device):
-	PRODUCT_DEVICE = "mido"
-	TARGET_KERNEL_CONFIG = f"{PRODUCT_DEVICE}_defconfig"
-
-register_device(XiaomiMidoDevice)
--- device/xiaomi/msm8953.py
+++
@@ -1,5 +0,0 @@
-from device.qcom.msm8996 import QcomMSM8996Device
-
-class XiaomiMSM8953Device(QcomMSM8996Device):
-	TARGET_ARCH = "arm64"
-	TARGET_KERNEL_SOURCE = "kernel/xiaomi/msm8953"
