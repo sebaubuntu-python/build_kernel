cfcf39f77655991d6c9a8fc82596afc1f235d6fa
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-11-22 19:17:41 +0100

    device: xiaomi: sm8250: Drop mikona
    * Delayed until Xiaomi stop spamming sm8250 devices into the market
    
    Change-Id: Ifc30db3755f3fe7fc513384d8ce5511973c2c5e0

--- device/xiaomi/mikona_a.py
+++
@@ -1,8 +0,0 @@
-from build_kernel.utils.device import register_device
-from device.xiaomi.sm8250 import XiaomiSM8250Device
-
-class XiaomiMikonaADevice(XiaomiSM8250Device):
-	PRODUCT_DEVICE = "mikona_a"
-	TARGET_KERNEL_CONFIG = "vendor/mikona_defconfig"
-
-register_device(XiaomiMikonaADevice)
--- device/xiaomi/mikona_vab.py
+++
@@ -1,9 +0,0 @@
-from build_kernel.utils.device import register_device
-from device.xiaomi.sm8250 import XiaomiSM8250Device
-
-class XiaomiMikonaVabDevice(XiaomiSM8250Device):
-	PRODUCT_DEVICE = "mikona_vab"
-	TARGET_KERNEL_CONFIG = "vendor/mikona_defconfig"
-	AB_OTA_UPDATER = True
-
-register_device(XiaomiMikonaVabDevice)
